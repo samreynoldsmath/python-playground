b1b265a5d5c0b638a5a193a7df5ea724e45126e3
Sam Reynolds <sreyn@proton.me>
2024-05-04 11:25:50 -0700

    use random mesh

--- src/contourf-test.py
+++ src/contourf-test.py
@@ -2,15 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    X, Y = grid()
+    x, y = coordinates()
+    X, Y = np.meshgrid(x, y)
     Z = f(X, Y)
     make_plot(X, Y, Z)
+    show_mesh(X, Y)
 
-def grid():
-    x = np.linspace(-1.0, 1.0, 100)
-    y = np.linspace(-1.0, 1.0, 100)
-    X, Y = np.meshgrid(x, y)
-    return X, Y
+def coordinates():
+    x = np.random.rand(100)
+    x = np.sort(x)
+    y = np.random.rand(100)
+    y = np.sort(y)
+    return x, y
 
 def f(X, Y):
     return np.sqrt(X**2 + Y**2)
@@ -19,6 +22,11 @@ def make_plot(X, Y, Z):
     fig, ax = plt.subplots()
     CS = ax.contourf(X, Y, Z)
     fig.colorbar(CS)
+    plt.show()
+
+def show_mesh(X, Y):
+    fig, ax = plt.subplots()
+    ax.plot(X, Y, 'o')
     plt.show()
 
 if __name__ == '__main__':

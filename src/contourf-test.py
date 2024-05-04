77b98ea9d034347b5d08152106b0760acfe2d471
Sam Reynolds <sreyn@proton.me>
2024-05-04 11:20:07 -0700

    modularize contourf-test.py

--- src/contourf-test.py
+++ src/contourf-test.py
@@ -2,11 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
+    X, Y = grid()
+    Z = f(X, Y)
+    make_plot(X, Y, Z)
+
+def grid():
     x = np.linspace(-1.0, 1.0, 100)
     y = np.linspace(-1.0, 1.0, 100)
     X, Y = np.meshgrid(x, y)
-    Z = np.sqrt(X**2 + Y**2)
+    return X, Y
 
+def f(X, Y):
+    return np.sqrt(X**2 + Y**2)
+
+def make_plot(X, Y, Z):
     fig, ax = plt.subplots()
     CS = ax.contourf(X, Y, Z)
     fig.colorbar(CS)

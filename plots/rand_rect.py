501089a769818fe3410bfceb8deeb77136c3199b
Sam Reynolds <sreyn@proton.me>
2024-05-07 10:26:27 -0700

    format with black

--- plots/rand_rect.py
+++ plots/rand_rect.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def main():
     x, y = coordinates()
     X, Y = np.meshgrid(x, y)
     Z = f(X, Y)
     make_plot(X, Y, Z)
     show_mesh(X, Y)
+
 
 def coordinates():
     x = np.random.rand(100)
@@ -15,8 +17,10 @@ def coordinates():
     y = np.sort(y)
     return x, y
 
+
 def f(X, Y):
     return np.sqrt(X**2 + Y**2)
+
 
 def make_plot(X, Y, Z):
     fig, ax = plt.subplots()
@@ -24,10 +28,12 @@ def make_plot(X, Y, Z):
     fig.colorbar(CS)
     plt.show()
 
+
 def show_mesh(X, Y):
     fig, ax = plt.subplots()
-    ax.plot(X, Y, 'o')
+    ax.plot(X, Y, "o")
     plt.show()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

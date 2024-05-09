761c000e15dbbae2a37e82f44bb30ef34d9028e8
Sam Reynolds <sreyn@proton.me>
2024-05-09 00:15:21 -0700

    add symbolic/tests

--- symbolic/edge.py
+++ symbolic/edge.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
 
-t = sp.Symbol("t")
+TWO_PI = 2 * np.pi
 
 
 class Edge:
+    t: sp.Expr = sp.symbols("t")
     t_bounds: tuple
     x_expr: sp.Expr
     y_expr: sp.Expr
@@ -14,24 +15,51 @@ class Edge:
 
     def __init__(
         self,
-        x_expr: sp.Expr,
-        y_expr: sp.Expr,
-        t_bounds: tuple[float, float] = (0.0, 2 * np.pi),
+        x: str,
+        y: str,
+        t_bounds: tuple[float, float] = (0.0, 2 * TWO_PI),
     ):
-        self.x_expr = x_expr
-        self.y_expr = y_expr
+        self.set_parameterization(x, y)
         self.t_bounds = t_bounds
+
+    def set_parameterization(self, x: str, y: str) -> None:
+        """
+        Define the edge parameterization using strings x,y each being an
+        expression in terms of t
+        """
+        for s in [x, y]:
+            self.validate_string_expression(s)
+        self.x_expr = sp.parse_expr(x)
+        self.y_expr = sp.parse_expr(y)
+
+    def validate_string_expression(self, s: str) -> None:
+        """
+        Raises a ValueError if the string is not a valid expression in terms of
+        t. Specifically, we require the string s to satisfy:
+        1. s contains no other variables except t (no "free symbols")
+        2. s contains no undefined expressions
+        """
+        expr = sp.parse_expr(s)
+        if expr.free_symbols and self.t not in expr.free_symbols:
+            raise ValueError(f"Unable to parse string: '{s}'")
 
     def sample(self, num_pts):
         t_vals = np.linspace(self.t_bounds[0], self.t_bounds[1], num_pts)
-        self.x = np.array([self.x_expr.subs(t, val) for val in t_vals])
-        self.y = np.array([self.y_expr.subs(t, val) for val in t_vals])
+        self.x = self.evaluate_expression(self.x_expr, t_vals)
+        self.y = self.evaluate_expression(self.y_expr, t_vals)
 
+    def evaluate_expression(self, expr: sp.Expr, t_vals: np.ndarray) -> np.ndarray:
+        vals = np.zeros(t_vals.shape)
+        for idx, t_val in enumerate(t_vals):
+            vals[idx] = float(expr.subs(self.t, t_val))
+        return vals
+
+def example(make_plot: bool=True):
+    edge = Edge("t*cos(t)", "t*sin(t)")
+    edge.sample(32)
+    if make_plot:
+        plt.plot(edge.x, edge.y, "k.-")
+        plt.show()
 
 if __name__ == "__main__":
-    x_expr = t * sp.cos(t)
-    y_expr = t * sp.sin(t)
-    edge = Edge(x_expr, y_expr)
-    edge.sample(100)
-    plt.plot(edge.x, edge.y)
-    plt.show()
+    example(make_plot=True)
---
+++ symbolic/tests/test_string_parser.py
@@ -0,0 +1,50 @@
+"""
+python3 -m pytest symbolic/tests -s
+"""
+import pytest
+
+from symbolic import Edge
+
+GOOD_STR = "t"
+
+def _test_good(good_str: str) -> None:
+    e = Edge(x=good_str, y=GOOD_STR)
+    e.sample(num_pts=8)
+    for x, y in zip(e.x, e.y):
+        assert isinstance(x, (float, int))
+        assert isinstance(y, (float, int))
+
+def _test_bad(bad_str: str) -> None:
+    with pytest.raises(ValueError):
+        Edge(x=bad_str, y=GOOD_STR)
+
+def test_good_str_cos() -> None:
+    _test_good(good_str="cos(t)")
+
+def test_good_str_gaussian() -> None:
+    _test_good(good_str="exp(-t**2)")
+
+def test_good_str_constant_literal() -> None:
+    _test_good(good_str="6.28")
+
+def test_good_str_constant_symbolic() -> None:
+    _test_good(good_str="2 * pi")
+
+def test_bad_str_x() -> None:
+    _test_bad(bad_str="x")
+
+def test_bad_str_sin_T() -> None:
+    _test_bad(bad_str="sin(T)")
+
+def test_bad_str_sim_t() -> None:
+    # _test_bad(bad_str="sim(t)")
+    #
+    # Unfortunately, it seems very difficult for sympy to recognize that "sim"
+    # is not a valid function. At least for now, we will have to settle for a
+    # TypeError being raised when the expression is evaluated.
+    #
+    # See symbolic/tests/traceback.txt for the full traceback.
+    edge = Edge(x="sim(t)", y=GOOD_STR)
+    with pytest.raises(TypeError) as e:
+        print(e)
+        edge.sample(num_pts=8)
